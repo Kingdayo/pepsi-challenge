71c11bb9936115baf9dfc76210abb578f345298b
unknown <sojapril16@gmail.com>
2022-12-29 04:20:12 +0100

    pepsi 1

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('.hamburger').addEventListener('click',function(){
-            this.classList.toggle('closeicon')
-            if(this.classList.contains('closeicon')){
-                document.querySelector('.mobilenav').style.cssText = `max-height:242px;opacity:1`
-            }
-            else{
-                document.querySelector('.mobilenav').style.cssText = `max-height:0px;opacity:0`
-            }
+        document.querySelector('.mobilenav').style.cssText = `height:100vh;opacity:1`
+    })
+
+    document.querySelector('.closenav').addEventListener('click',function(){
+        document.querySelector('.mobilenav').style.cssText = `height:0vh;opacity:0`
     })
 })
